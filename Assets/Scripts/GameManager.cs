66532ce07e6e141c07a9707a8d050e3f58ded6a0
v0id <t@ti2ga.de>
2023-11-18 16:02:29 +0100

    added cam manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float rageValue;
-    public static GameManager Instance { get; private set; }
+    [SerializeField] private CamManager camManager;
 
+    public static GameManager Instance { get; private set; }
 
     private void Awake()
     {
         if (Instance != null) Debug.LogWarning("There is more than one GameManager!");
         Instance = this;
+
+        camManager.SetInGame(true);
     }
+
 
     public float GetRageValue()
     {
